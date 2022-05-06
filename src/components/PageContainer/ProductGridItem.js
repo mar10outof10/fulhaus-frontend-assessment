2d3fe9000690967133dabb1729593e2a78c1632c
Martin Halas <marhaldev@gmail.com>
2022-05-05 22:38:17 -0400

    Further adjusting product grid items

--- src/components/PageContainer/ProductGridItem.js
+++ src/components/PageContainer/ProductGridItem.js
@@ -1,18 +1,18 @@
 import "./ProductGridItem.scss";
+import itemPicture from "../../assets/images/productGrid__itemPicture.png"
 
 const ProductGridItem = () => {
   return (
     <div className="productGridItem">
-      <div className="productGridItem__container--active">
-        <div className="productGridItem__overlay">
-          <div className="productGridItem__overlayUpperSection">
-            <div className="productGridItem__productName"></div>
-            <div className="productGridItem__brandName"></div>
-          </div>
-          <div className="productGridItem__overlayLowerSection">
-            <button className="productGridItem__addToCart"></button>
-            <div className="productGridItem__price"></div>
-          </div>
+      <img className="productGridItem__image" src={itemPicture} alt="product item picture" />
+      <div className="productGridItem__overlay">
+        <div className="productGridItem__overlayUpperSection">
+          <div className="productGridItem__productName">Product Name</div>
+          <div className="productGridItem__brandName">BRAND NAME</div>
+        </div>
+        <div className="productGridItem__overlayLowerSection">
+          <button className="productGridItem__addToCart">+ Add to Cart</button>
+          <div className="productGridItem__price">$900</div>
         </div>
       </div>
     </div>
