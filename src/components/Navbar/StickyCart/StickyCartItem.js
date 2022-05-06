f0674b3ea41caa1d6824131ed36b9171def1bbf3
Martin Halas <marhaldev@gmail.com>
2022-05-06 00:01:30 -0400

    Styled sticky cart items

--- src/components/Navbar/StickyCart/StickyCartItem.js
+++ src/components/Navbar/StickyCart/StickyCartItem.js
@@ -1,17 +1,22 @@
 import "./StickyCartItem.scss";
+import itemPicture from "../../../assets/images/productGrid__itemPicture.png"
+import closeIcon from "../../../assets/images/closeIcon.svg"
+
 
 const StickyCartItem = () => {
   return (
     <div className="stickyCartItem">
       <div className="stickyCartItem__imageContainer">
-        <img className="stickyCartItem__image" />
+        <img className="stickyCartItem__image" src={itemPicture} alt="product item picture" />
       </div>
       <div className="stickyCartItem__textContainer">
-        <div className="stickyCartItem__productName"></div>
-        <div className="stickyCartItem__brandName"></div>
-        <div className="stickyCartItem__price"></div>
+        <div className="stickyCartItem__productName">Product Name</div>
+        <div className="stickyCartItem__brandName">BRAND NAME</div>
+        <div className="stickyCartItem__price">$900</div>
       </div>
-      <button className="stickyCartItem__closeButton"></button>
+      <button className="stickyCartItem__closeButton">
+        <img className="stickyCartItem__closeButtonIcon" src={closeIcon} alt="X"/>
+      </button>
     </div>
   );
 };
